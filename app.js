c638bf44353adf6d5bebcb304a713a0cb0d386ae
alexandria-perry <aeperry22@gmail.com>
2018-11-26 15:09:17 -0800

    ReactComplete

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
 // TODO
+var GroceryList = (props) => (
+  <ul> 
+    {props.list.map(lists =>
+      <GroceryListItems lists={lists} />
+      )}
+  </ul>
+  
+)
+
+class GroceryListItems extends React.Component{
+  constructor(props){
+    super(props)
+
+    this.state ={
+      done: false
+  };
+}
+
+  MouseOver(){
+    this.setState({
+    done: !this.state.done
+  });
+}
+  render(){
+      var style = {
+        fontWeight: this.state.done ? 'bold' : 'normal'
+  };
+
+      return(
+      <li style={style} onMouseOver= {this.MouseOver.bind(this)}> {this.props.lists}</li> 
+  
+    )
+
+    }
+
+}
+
+
+ReactDOM.render(<GroceryList list ={['Cucumber', 'Kale']} />, document.getElementById("app"));
